7c87909586200a2a638dbc42685863c5bf9e5a6c
Ankit Mishra <mechturncoder@gmail.com>
2024-01-26 11:39:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function page4Animation(){
+    let containerEl = document.querySelector("#elem-container");
+let fixedImage = document.querySelector("#fixed-image");
+
+containerEl.addEventListener('mouseenter', function(){
+    fixedImage.style.display = "block";
+    // alert("hello")
+})
+containerEl.addEventListener('mouseleave', function(){
+    fixedImage.style.display = 'none'
+})
+
+let elems = document.querySelectorAll('.elem');
+
+elems.forEach(function(e){
+    e.addEventListener("mouseenter", function(){
+        let image = e.getAttribute("data-image")
+        fixedImage.style.backgroundImage = `url(${image})`
+    })
+})
+}
+
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+}
+swiperAnimation();
+page4Animation()
